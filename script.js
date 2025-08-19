246dc219013c2da4e420158097e757966e347ad5
weeksl123 <weeksl@stu.otc.school.nz>
2025-08-19 19:18:39 +1200

    Added policies page

--- script.js
+++ script.js
@@ -2,23 +2,14 @@ document.getElementById("booking_button").addEventListener("click", function() {
     window.location.href = "./booking.html";
 }); 
 
-// document.getElementById("booking_form").addEventListener("submit", async function(e) {
-//     e.preventDefault();
 
-//     const formData = new FormData(this);
+document.querySelectorAll(".dropdown").forEach(dropdown => {
+    const header = dropdown.querySelector(".dropdown-header");
+    const toggleBtn = dropdown.querySelector(".toggle-btn");
+        
+    header.addEventListener("click", () => {
+        dropdown.classList.toggle("active");
 
-//    try {
-//        const response = await fetch('save_booking.php', {
-//            method: "POST",
-//            body: formData
-//        });
-
-//        if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}`);
-
-//        const result = await response.json();
-//        alert(result.message || "Booked successfully!");
-//    } catch (error) {
-//        console.error("Error during booking:", error);
-//        alert("Error during booking:", error);
-//    }
-// });
+        toggleBtn.textContent = dropdown.classList.contains("active") ? "x" : "+";
+    });
+});
